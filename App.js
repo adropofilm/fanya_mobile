f68b381c62177d633edeaeaa6415b1d4f75c9d20
Fatima Mohamed <adropofilm@gmail.com>
2023-01-08 14:09:53 -0600

    Setup repo with expo + add gitignore

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TaskListContainer from './src/components/TaskListContainer/TaskListContainer';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TaskListContainer></TaskListContainer>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
